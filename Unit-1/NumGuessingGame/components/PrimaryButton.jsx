b57770fbc7f3c8812bbe5e019d45f6552e3650b9
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-01 21:05:02 -0700

    Added custom button component

---
+++ Unit-1/NumGuessingGame/components/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import { Pressable, View, StyleSheet, Text } from "react-native";
+import React from "react";
+export default function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
--- Unit-1/NumGuessingGame/screens/StartGameScreen.js
+++ Unit-1/NumGuessingGame/screens/StartGameScreen.js
@@ -1,18 +1,13 @@
-import {
-  TextInput,
-  Text,
-  View,
-  StyleSheet,
-  Pressable
-} from "react-native";
+import { TextInput, Text, View, StyleSheet, Pressable } from "react-native";
 import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
     <>
       <View>
-        <TextInput />
-        
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
       </View>
     </>
   );
