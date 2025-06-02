14d41364166f9f4460d58aaf9e0690bb6ae60f14
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-01 21:20:35 -0700

    Added shadows for android and iOS

--- Unit-1/NumGuessingGame/screens/StartGameScreen.js
+++ Unit-1/NumGuessingGame/screens/StartGameScreen.js
@@ -5,10 +5,28 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <>
-      <View>
+      <View style={styles.inputContainer}>
+        <TextInput />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: "20%",
+    marginHorizontal: 24,
+    backgroundColor: "#f48b7b",
+    borderRadius: 8,
+    //shadow for android
+    elevation: 4,
+    //show for iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 1
+  }
+});
