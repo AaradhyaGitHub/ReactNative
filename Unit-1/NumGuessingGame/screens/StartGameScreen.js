571139e729ac9c5e4ec027582058fa31ad49299c
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-01 20:24:11 -0700

    Added starting component screens

---
+++ Unit-1/NumGuessingGame/screens/StartGameScreen.js
@@ -0,0 +1,19 @@
+import {
+  TextInput,
+  Text,
+  View,
+  StyleSheet,
+  Pressable
+} from "react-native";
+import React from "react";
+
+export default function StartGameScreen() {
+  return (
+    <>
+      <View>
+        <TextInput />
+        
+      </View>
+    </>
+  );
+}
