91529301ef33e04dfa8f5b940cad851fa615c246
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-02 22:02:21 -0700

    Added pressable custom buttons

--- Unit-1/NumGuessingGame/components/PrimaryButton.jsx
+++ Unit-1/NumGuessingGame/components/PrimaryButton.jsx
@@ -1,9 +1,43 @@
 import { Pressable, View, StyleSheet, Text } from "react-native";
 import React from "react";
 export default function PrimaryButton({ children }) {
+  function pressHandler() {
+    console.log(`${children} button pressed`);
+  }
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.outerButtonContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.innerbuttonContainer, styles.pressed]
+            : styles.innerbuttonContainer
+        }
+        android_ripple={{ color: "#fdca00" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  outerButtonContainer: {
+    borderRadius: 59,
+    overflow: "hidden"
+  },
+  innerbuttonContainer: {
+    paddingVertical: 2,
+    paddingHorizontal: 9,
+    backgroundColor: "#ffe375",
+    elevation: 4
+  },
+  buttonText: {
+    fontSize: 32,
+    color: "#4a4a4a",
+    textAlign: "center"
+  },
+  pressed: {
+    opacity: 0.5
+  }
+});
--- Unit-1/NumGuessingGame/screens/StartGameScreen.js
+++ Unit-1/NumGuessingGame/screens/StartGameScreen.js
@@ -6,9 +6,18 @@ export default function StartGameScreen() {
   return (
     <>
       <View style={styles.inputContainer}>
-        <TextInput />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+        <TextInput
+          style={styles.numInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>↻</PrimaryButton>
+          <PrimaryButton>✓</PrimaryButton>
+
+        </View>
       </View>
     </>
   );
@@ -18,6 +27,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     padding: 16,
     marginTop: "20%",
+    alignItems: "center",
     marginHorizontal: 24,
     backgroundColor: "#f48b7b",
     borderRadius: 8,
@@ -27,6 +37,26 @@ const styles = StyleSheet.create({
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
-    shadowOpacity: 1
+    shadowOpacity: 0.225
+  },
+
+  numInput: {
+    height: 62,
+    width: "20%",
+    fontSize: 32,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "#f4dc7b",
+    backgroundColor: "#f4dc7b2d",
+    color: "#fde950d5",
+    marginVertical: 1,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignItems:'center',
+    padding: 20,
+    gap:20
   }
 });
