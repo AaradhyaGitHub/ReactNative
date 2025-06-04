e8751985c2784aeec6057026eeee51af2de93690
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-04 13:56:13 -0700

    Added game over screen and more custom image designs

--- Unit-1/NumGuessingGame/screens/GameOverScreen.jsx
+++ Unit-1/NumGuessingGame/screens/GameOverScreen.jsx
@@ -1,9 +1,39 @@
+// @ts-nocheck
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Title from "../components/ui/Title";
 export default function GameOverScreen() {
   return (
-    <View>
-      <Text>Game Over!</Text>
+    <View style={styles.rootContainer}>
+      <Title>Game Over!!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.imageStyle}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your Phone needed X rounds to guess the number Y</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "white",
+    overflow: "hidden",
+    margin: 36
+  },
+  imageStyle: {
+    width: "100%",
+    height: "100%"
+  }
+});
