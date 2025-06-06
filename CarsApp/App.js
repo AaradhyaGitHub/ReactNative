254cf217db610e49bff3cdd19c99a89debad85f8
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-06 16:30:10 -0700

    Added nested drawer navigation within stack navigation

--- CarsApp/App.js
+++ CarsApp/App.js
@@ -5,15 +5,29 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 //Custom Compoents
 import CategoriesScreen from "./screens/CategoriesScreen";
 import CarsOverviewScreen from "./screens/CarsOverviewScreen";
 import CarDetailPage from "./screens/CarDetailPage";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 // ------------------------[END IMPORTS]------------------------ //
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Categories" component={CategoriesScreen}/>
+        <Drawer.Screen name="Favorites" component={FavoritesScreen}/>
+      </Drawer.Navigator>
+    </>
+  );
+}
 
 export default function App() {
   return (
@@ -21,7 +35,7 @@ export default function App() {
       <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName="Categories"
+          initialRouteName="Drawer"
           screenOptions={{
             headerStyle: { backgroundColor: "#29525187" },
             contentStyle: { backgroundColor: "#c1c1c1ef" },
@@ -29,28 +43,15 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="Categories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={DrawerNavigator} // switched from Catergories component
             options={{
               title: "All Categories"
             }}
           />
-          <Stack.Screen
-            name="CarsOverview"
-            component={CarsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   // @ts-ignore
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId
-            //   };
-            // }}
-          />
-          <Stack.Screen
-            name="CarDetail"
-            component={CarDetailPage}
-            
-          />
+
+          <Stack.Screen name="CarsOverview" component={CarsOverviewScreen} />
+          <Stack.Screen name="CarDetail" component={CarDetailPage} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ CarsApp/screens/FavoritesScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Expo and React Native
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function FavoritesScreen() {
+  return (
+    <>
+      <View>
+        <Text>FavoritesScreen</Text>
+      </View>
+    </>
+  );
+}
