758dd237faf26788193cf1e6bbb8430f85e703b0
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-05 17:04:00 -0700

    Added dependencies for navigation and crated Stack Screen setup

--- CarsApp/App.js
+++ CarsApp/App.js
@@ -1,13 +1,27 @@
+import React from "react";
+
+//Expo and React Native
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//Custom Compoents
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+// ------------------------[END IMPORTS]------------------------ //
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
