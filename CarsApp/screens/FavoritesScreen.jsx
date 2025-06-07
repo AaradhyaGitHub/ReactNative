c2e9a45265ff9a84a3f3c0113e735ee3d00cfd9d
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-06 22:34:47 -0700

    Rendering No favorite message conditinally

--- CarsApp/screens/FavoritesScreen.jsx
+++ CarsApp/screens/FavoritesScreen.jsx
@@ -1,11 +1,45 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { Text, View, StyleSheet } from "react-native";
 //Expo and React Native
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
 import CarItemCard from "../components/CarItemCard";
+import { FavoritesContext } from "../store/redux/favorites-context";
+import { CARS } from "../data/dummy-data";
 
 export default function FavoritesScreen() {
-  return <>
-  </>;
+  const favoriteCarCtx = useContext(FavoritesContext);
+  const favoriteCars = CARS.filter((car) =>
+    // @ts-ignore
+    favoriteCarCtx.ids.includes(car.id)
+  );
+  return (
+    <>
+      {favoriteCars.length === 0 ? (
+        <View style={styles.rootContainer}>
+          <Text style={styles.defaultText}>
+            No <Text style={styles.highlight}>cars</Text> added :(
+          </Text>
+        </View>
+      ) : (
+        <CarItemCard data={favoriteCars} />
+      )}
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '70%'
+  },
+  defaultText: {
+    flex: 1.5,
+    textAlign: "center",
+    fontSize: 32,
+    color: "#818181"
+  },
+  highlight: {
+    color: "#9d0830"
+  }
+});
