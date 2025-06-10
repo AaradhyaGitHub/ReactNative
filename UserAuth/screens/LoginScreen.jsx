2eabf37d2d37b0061cd1219a8cf9b0efdf78d65c
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-10 16:41:18 -0700

    Replicated logic for Logging in

--- UserAuth/screens/LoginScreen.jsx
+++ UserAuth/screens/LoginScreen.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { signInUser } from "../util/auth";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isSigningOn, setIsSigningOn] = useState(false);
+
+  async function signInHandler({ email, password }) {
+    setIsSigningOn(true);
+    await signInUser(email, password);
+    setIsSigningOn(false);
+  }
+
+  if (isSigningOn) {
+    return <LoadingOverlay message="Logging In..." />;
+  }
+  return <AuthContent isLogin  onAuthenticate={signInHandler}/>;
 }
 
 export default LoginScreen;
