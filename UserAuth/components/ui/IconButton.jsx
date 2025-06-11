71a0caaaffef1ab0b0c3f95bc11bce5aaa9fa607
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-06-10 20:07:14 -0700

    Added logout icon and implemented logout feature

--- UserAuth/components/ui/IconButton.jsx
+++ UserAuth/components/ui/IconButton.jsx
@@ -18,8 +18,8 @@ export default IconButton;
 
 const styles = StyleSheet.create({
   button: {
-    margin: 8,
-    borderRadius: 20
+    padding: 0,
+    borderRadius: 20,
   },
   pressed: {
     opacity: 0.7
